fed7556b108ddd93ca03cd905b2c11965bab7109
Ariel Ferreyra <arielgerman293@gmail.com>
2022-12-29 23:35:52 -0300

    primer trabajo

---
+++ app.js
@@ -0,0 +1,15 @@
+//quiero calcular el iva de una entrada, mostrarle al usuario el iva
+//sumar la entrada + su iva y mostrar al usuario
+//restarle el descuento del 5% porque pago en efectivo
+//sumar el interes del 10% por pagar con tarjeta
+
+function sumaTotalDeEntradas(entrada1=0, entrada2=0, entrada3=0){
+    return entrada1 + entrada2 + entrada3
+}
+
+let entrada1 = Number(prompt("primer precio"))
+let entrada2 = Number(prompt("primer precio"))
+let entrada3 = Number(prompt("primer precio"))
+            function sumarTotalDeEntradas()
+let entradasPrecioFinal = sumaTotalDeEntradas(entrada1, entrada2, entrada3)
+alert(entradasPrecioFinal)
